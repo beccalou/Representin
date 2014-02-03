d77ce695dcc757c05d09f99a2ae1e669bea7f5ad
Becky Bell <rlbell@alum.bu.edu>
2014-02-03 13:23:35 -0500

    added annotate gem

--- YouCheckandBalance/app/models/phrase.rb
+++ YouCheckandBalance/app/models/phrase.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: phrases
+#
+#  id      :integer          not null, primary key
+#  user_id :integer
+#
+
 class Phrase < ActiveRecord::Base
 	belongs_to :users
 end
