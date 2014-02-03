39cc332dbb66c5895ecb9f75ae130ca4814121f5
Becky Bell <rlbell@alum.bu.edu>
2014-02-03 11:39:53 -0500

    generated Devise views

--- YouCheckandBalance/db/migrate/20140203160749_devise_create_users.rb
+++ YouCheckandBalance/db/migrate/20140203160749_devise_create_users.rb
@@ -37,6 +37,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
+    add_index :users, :first_name, :last_name, :address
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
