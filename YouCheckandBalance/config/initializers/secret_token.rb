4e873824e7de0b4bfcaa616dea0638415cb76dde
Becky Bell <rlbell@alum.bu.edu>
2014-02-03 10:04:26 -0500

    created new rails application named YouCheckandBalance

---
+++ YouCheckandBalance/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YouCheckandBalance::Application.config.secret_key_base = 'bce362580a03ae22facda16b64a664b92b8ca26bec8265c23a0ab145b1896e5f3cfb55d3ad2c7d2afd930495f8f2ae8e363e8fe9a7de7643b115628d700bdea1'
