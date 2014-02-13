28b2d2db58c2918c417bbdf20cc7be5a4e32d72e
Becky Bell <rlbell@alum.bu.edu>
2014-02-13 17:10:53 -0500

    added bullet gem to Gemfile

--- Gemfile
+++ Gemfile
@@ -59,6 +59,8 @@ group :development, :test do
   gem 'guard-rspec', require: false
 end
 
+gem "bullet", :group => "development"
+
 gem 'simplecov', require: false, group: :test
 
 # Use ActiveModel has_secure_password
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,4 +29,14 @@ YouCheckandBalance::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+  config.after_initialize do
+  Bullet.enable = true
+  Bullet.alert = true
+  Bullet.bullet_logger = true
+  Bullet.console = true
+  Bullet.rails_logger = true
+  Bullet.airbrake = true
+  Bullet.add_footer = true
+end
+
 end
